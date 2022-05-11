f756ba33d31100ffa66c22017079ba50ee49d515
AasifFiraz <aasiffiraz178@gmail.com>
2022-05-11 23:02:55 +0530

    Added callback functions for displaying user friendly data

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
 
@@ -22,6 +22,35 @@ def create_tables():
 
 
 jwt_token = JWTManager(app=app)  # does not create /auth, we have to do manually
+
+
+@jwt_token.expired_token_loader
+def expired_token_callback():
+    return jsonify({"description": "Token is expired", "error": "expired token"})
+
+
+# When the user enters an invalid jwt token
+@jwt_token.invalid_token_loader
+def invalid_token_callback(error):
+    return jsonify({"description": "Signature verification failed", "error": "invalid token"}), 401
+
+
+# When JWT token is not passed in the header
+@jwt_token.unauthorized_loader
+def unauthorized_callback(error):
+    return jsonify({"description": "Request does not contain an access token", "error": "no token"})
+
+
+@jwt_token.needs_fresh_token_loader
+def needs_fresh_token_callback():
+    return jsonify({"description": "Please login with your credentials and pass the access token",
+                    "error": "fresh token required"})
+
+
+@jwt_token.revoked_token_loader
+def revoked_token_callback():
+    return jsonify({"description": "The token has been revoked", "error": "revoked_token"})
+
 
 api.add_resource(Item, '/item/<string:name>')  # same as @app.route("/item/<string:name>")
 api.add_resource(ItemList, '/items')
